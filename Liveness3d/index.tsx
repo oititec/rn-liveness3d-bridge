748f15b7a5725fc363852286e568a8bc3c84bf18
catelli-gok <gabriel.catelli@gok.digital>
2022-10-18 07:25:30 -0300

    remove responde ex file

--- Liveness3d/index.tsx
+++ Liveness3d/index.tsx
@@ -8,20 +8,3 @@ export function startLiveness3d(appKey: string) {
   return Liveness3dReactNative.startliveness3d(appKey);
   //console.log(NativeModules);
 }
-
-export async function resultFaceCaptcha(appKey: string): Promise<string> {
-  const dataReq = {appkey: appKey};
-
-  const response = await fetch(
-    'https://comercial.certiface.com.br:8443/facecaptcha/service/captcha/document/result',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: qs.stringify(dataReq),
-    },
-  );
-  const data = await response.json();
-  return data.facecaptcha;
-}
